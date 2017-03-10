c7ffc56f8eca31e6130e0bc2ade8e2c04338c968
Fadwa <fadwa.fawzy@gmail.com>
2017-03-10 18:07:08 +0100

    modify

--- run_analysis.R
+++ run_analysis.R
@@ -20,66 +20,100 @@ trainsubject <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 testDim <- dim(testdata)
 trainDim <- dim(traindata)
 
-## conmbine subject variable
-subjectID <- data.table(rbind(testsubject, trainsubject))
-
-## add testFlag variable which is TRUE if the observation from the testdata and FALSE otherwise
-## add to the test table
-testdata$testFlag <- rep(TRUE,testDim[1])
-## add to the train table
-traindata$testFlag <- rep(FALSE,trainDim[1])
-
-## Combine the train and test tables, add the subjectID
+## Combine the `testdata` and `traindata` tables. Then, add the `subjectID`
 totalData <- rbind(testdata,traindata)
-totalData <- cbind(totalData, setnames(subjectID,c('subjectID')))
+
+##Check the dimensions of the final big dataset `totalData`.
+dim(totalData)
 
 # Extract the mean and std (part 2) ------------------------------------------------------
-
-## get the indices for the required variable
+## get varInd
 varInd <- grep("mean|std", featureNames[,2])
+numExtractedVariables <- length(varInd)
 
-## Extract the corresponding measurements
+## check the variables names
+featureNames[varInd,2]
+
+
+## Extract the corresponding measurements from the merged table `totalData`
 meanStd_data <- totalData[,varInd]
+head(meanStd_data)
+
+## Combine subject variable from test data and training data into one column `subjectID`.
+subjectID <- data.table(rbind(testsubject, trainsubject))
+dim(subjectID)
+
+
+## Create `testFlag` variable
+testFlag <- data.table(rep(c(T,F), c(testDim[1],trainDim[1])))
+dim(testFlag)
+
+
+## Combine `subjectID` and `testFlag` variables to `meanStd_data`
+meanStd_data <- cbind(meanStd_data, setnames(subjectID,c('subjectID')), 
+                      setnames(testFlag,c('testFlag')))
+
+## check the dimensions for the meanStd_data
+dim(meanStd_data)
+
 
 
 # Name the activities part(3) -----------------------------------------------------------------
 
-## create a function that replace number with names
+## Create a function `replaceNumberWithName` that replace activity number with 
+## activity names.Then apply it on the testlabels and the trainlabels.
 replaceNumberWithName <- function(x){activityNames[x,2]}
+
 ## replace number with names for test data
 testlabels_names <- sapply(testlabels,replaceNumberWithName)
+
 ## replace number with names for train data
 trainlabels_names <- sapply(trainlabels,replaceNumberWithName)
 
-## conmbine activity labels
-combinedactivity <- rbind(testlabels_names, trainlabels_names)
 
-## add the label variable to the totalData
-totalData$activity <- combinedactivity
+## conmbine activity labels into a single column combinedactivity.
+combinedactivity <- data.table(rbind(testlabels_names, trainlabels_names))
 
 
-# labels the data set with descriptive variable names (part 4) --------------------------
+## add the label variable to the `meanStd_data`
+meanStd_data <- cbind(meanStd_data, setnames(combinedactivity,c('activity')))
 
-setnames(totalData,colnames(totalData)[1:561],as.character(featureNames[,2]))
+## check meanStd_data dimensions
+dim(meanStd_data)
 
-# 3D dataset (part 5) -----------------------------------------------------
+## Check the new names
+names(meanStd_data)
 
-#From the data set in step 4, creates a second, independent tidy data set with the 
-#average of each variable for each activity and each subject.
 
-library(abind)
+# label the dataset with descriptive variable names (part 4) --------------
 
-## take a look at the trainsubject and testsubject data
+## old column names
+names(meanStd_data)
+
+## set the new names. VarInd from step #2
+setnames(meanStd_data,colnames(meanStd_data)[1:numExtractedVariables],as.character(featureNames$V2[varInd]))
+
+## new column names
+names(meanStd_data)
+
+# Tidy dataset (part 5) -----------------------------------------------------
+
+## take a look at the `trainsubject` and `testsubject` data. 
 table(trainsubject)
 table(testsubject)
 
-## aggregate wrt subjectID and activity name
-TidyData <- aggregate(totalData[,1:561], by=list("id"=totalData$subjectID, "activity" = totalData$activity), mean)
+## use `aggregate` to calculate the `mean` for each measurement grouped by 
+## `subjectID` and `activity`. The final dataset `TidyData` has dimensions of
+## [180x563]. 
 
-## check tidyData dimension
+
+
+TidyData <- aggregate(meanStd_data[,1:numExtractedVariables], by=list("id"=meanStd_data$subjectID, "activity" = meanStd_data$activity), mean)
+
+## check tidyData dimension. The output variables (columns) are id for subject id, 
+## activity, testFlag, and the 561 measurements. 
 dim(TidyData)
 
+## Save the `TidyData` as a `.txt` file.
 write.table(TidyData,"TidyData.txt", row.names = F)
-
-
 
